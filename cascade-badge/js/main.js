4129c87a1972f06da2108f7e5e9a8ce3eb9a2e30
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-05 10:52:02 -0700

    Finished second array challenge

--- cascade-badge/js/main.js
+++ cascade-badge/js/main.js
@@ -7,6 +7,20 @@ reverseList(['Mew', 'Mewtoo', 'Pikachu', 'Ditto' ])
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
 
+const isGreater = arrays => {
+    let array1 = arrays[0].map(x => x**2)
+    let array2 = arrays[1].map(x => x**3)
+    if(array1.reduce((acc, curr) => acc+curr, 0) > array2.reduce((acc, curr) => acc+curr, 0)){
+        return true
+    }else{
+        return false
+    }
+
+
+}
+
+console.log(isGreater([[3,10],[2,2]]))
+
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
