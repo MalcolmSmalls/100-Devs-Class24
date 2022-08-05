149eedd13ad52639ddbac43a9b3e75ac904c536d
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-05 11:00:49 -0700

    Finished fourth array challenge

--- cascade-badge/js/main.js
+++ cascade-badge/js/main.js
@@ -26,4 +26,17 @@ console.log(isGreater([[3,10],[2,2]]))
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
+const isMultiple = array => {
+    let newArr = []
+    array.forEach((x,i) => {
+        if(x%i === 0){
+            newArr.push(x)
+        }
+    })
+    return newArr
+}
+
+console.log(isMultiple([68, -1, 1, -7, 10, 10]))
+
+
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
